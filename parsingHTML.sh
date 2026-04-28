ff84430fb023959b289783519b054ae0f040c39b
João Victor <107276396+JoaoVicLisboa@users.noreply.github.com>
2026-04-28 18:39:48 -0300

    Update parsin
    Ajustei para que seja impresso os subdominios apresentados no html e coloquei para imprimir o IP

--- parsingHTML.sh
+++ parsingHTML.sh
@@ -6,10 +6,20 @@ then
 	echo "Exemplo: $0 google.com"
 else
 	wget -nv -O /tmp/$1.http.html $1 
-	
-	cat /tmp/$1.http.html | grep href | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<li" >> /tmp/$1.url.txt
-for url in  $[ cat /tmp/$ip.url.txt ]
+	echo "#####################################"
+	echo "->        Buscando Hosts . . .     <-"
+	echo "#####################################"
+
+	cat /tmp/$1.http.html | grep href | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | grep -v "<li" > /tmp/$1.url.txt
+for url in  $( cat /tmp/$1.url.txt )
 do
 	echo "$url"
+	host $url | grep -v "NXDOMAIN"  >>  /tmp/$1.host.txt
 done
+	echo "####################################"
+	echo "->       Resolvendo hosts . . .   <-"
+	echo "####################################"
+	paste -d ' has adress ' /tmp/$1.url.txt /tmp/$1.host.txt
+	echo "" > /tmp/$1.host.txt
+
 fi
